3c5dfa941889c077152ee6b80e6d99df4476191d
ayush2202ksr12111 <143083048+ayush2202ksr12111@users.noreply.github.com>
2025-07-26 14:51:17 +0530

    Update opt_emp.sql
    adding data of employee

--- opt_emp.sql
+++ opt_emp.sql
@@ -8,3 +8,20 @@ CREATE TABLE Opt__emp(
     Opt__emp_Age INT CHECK (Age >= 0 AND Age <= 99),
     Opt__emp_contact_Phone int(10)
 );
+
+----- Inserting employee details or sample details of employee for now location we are considering noida you can choose other also
+INSERT INTO Opt__emp(
+Opt__emp_ID,
+    Opt__emp_First_Name,
+    Opt__emp_Last_Name,
+    Opt__emp_Location,
+    Opt__emp_Age,
+    Opt__emp_contact_Phone
+) VALUES (990022321,'Ayush','Jain','Noida',26,9625463432),
+    (990022322,'Rishabh','Pundir','Noida',2,7278932543),
+    (990022323,'Aksha','Verma','Noida',22,7456472343),
+    (990022324,'Akash','Pandey','Noida',22,7489309879),
+    (990022325,'Srishti','Gupta','Noida',25,7378909892),
+    (990022326,'Sonu','Sharma','Noida',27,9089239890),
+    (990022327,'Rahul','Sharma','Noida',26,7378909565);
+
